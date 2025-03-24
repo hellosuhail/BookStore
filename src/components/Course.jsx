46af1ca20d8136967c328b8fe29209f2aa13b68e
unknown <https://github.com/hellosuhail>
2025-03-24 09:49:09 +0530

    first commit

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -23,7 +23,7 @@ const res= await axios.get('http://localhost:4000/card')
   
   return (
     <div>
-        <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 ">
+        <div className="max-w-screen-2xl container mx-auto  md:px-20 px-4 ">
             <div className="mt-18 justify-center text-center items-center">
     <h1 className="text-2xl md:text-4xl">We'r deligheted to have you <span className="text-[#366679]"> here! :)</span></h1>
     <p className="mt-12">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt quibusdam aliquam veniam nulla, minus vitae rerum recusandae. Neque quaerat aperiam necessitatibus libero alias molestiae dolores! Eius explicabo corrupti saepe aut, velit perferendis id esse, necessitatibus, molestiae repellat perspiciatis dolores accusamus aperiam! Consectetur, corporis cum hic facere facilis molestias sit assumenda quis distinctio similique odio.</p>
